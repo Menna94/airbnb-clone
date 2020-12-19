0c6aaac846023b24594f39e02c5b156f00ec83fc
maher elsayed <maher.elsayed.dev@gmail.com>
2020-12-19 11:47:12 +0200

    merge all branches

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import './components/Gift/Gift'
-import HomeScreen from './screens/HomeScreen';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./styles.scss";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
-  return (
-    <HomeScreen/>
-  );
+    return (
+        <Router>
+            <Route path="/" component={HomeScreen} exact />
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/screens/HomeScreen.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Gift from '../components/Gift/Gift'
-
-export default function HomeScreen() {
-    return (
-      <Gift/>
-    )
-}
