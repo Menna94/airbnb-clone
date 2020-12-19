16d008d11d015f4bdd10e3fa7cb3daf30eef9e9b
maher elsayed <maher.elsayed.dev@gmail.com>
2020-12-19 11:29:06 +0200

    fix merging conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./styles.scss";
@@ -9,18 +8,6 @@ function App() {
             <Route path="/" component={HomeScreen} exact />
         </Router>
     );
-=======
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import './components/Gift/Gift'
-import HomeScreen from './screens/HomeScreen';
-
-
-function App() {
-  return (
-    <HomeScreen/>
-  );
->>>>>>> 1e902e2208d137fcebf1d23219c15a69f915b2fd
 }
 
 export default App;
