892b213a5db140def4416b6b41f9174136653349
mu9tafa <mu9tafa.28@gmail.com>
2020-12-24 15:07:30 +0200

    adding dashboard screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './components/Gift/Gift'
 import HomeScreen from './screens/HomeScreen';
+import DashboardScreen from './screens/DashboardScreen';
 
 
 function App() {
   return (
-    <HomeScreen/>
+    // <HomeScreen/>
+    <DashboardScreen />
   );
 }
 
