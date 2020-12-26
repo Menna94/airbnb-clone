82192c02e3f3515b347b8c41e06135c0261af368
maher elsayed <maher.elsayed.dev@gmail.com>
2020-12-26 14:37:42 +0200

    fix conflicts in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import './components/Gift/Gift'
-import HomeScreen from './screens/HomeScreen';
-import DashboardScreen from './screens/DashboardScreen';
-
-
-function App() {
-  return (
-    // <HomeScreen/>
-    <DashboardScreen />
-  );
-=======
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
-import HostScreen from './screens/HostScreen';
-import SearchPage from './screens/SearchPage';
-import Propertieslist from './components/Properties/Propertieslist';
-            
+import HostScreen from './screens/HostScreen'
+import SearchPage from './screens/SearchPage'
+import Propertieslist from './components/Properties/Propertieslist'
+
 function App() {
     return (
         <Router>
@@ -26,11 +12,10 @@ function App() {
                 <Route path="/" component={HomeScreen} exact />
                 <Route path="/search" component={SearchPage} />
                 <Route path="/host" component={HostScreen} exact />
-                <Route path="/properties" component={Propertieslist} exact /> 
+                <Route path="/properties" component={Propertieslist} exact />
             </Switch>
         </Router>
     )
->>>>>>> main
 }
 
 export default App
